61aec960b88cf51ed2fe386b8230826c5ab14f20
jacobestrich <144174354+jacobestrich@users.noreply.github.com>
2023-11-03 09:57:53 -0700

    Delete FavoriteProgrammingLang.java

--- FavoriteProgrammingLang.java
+++
@@ -1,8 +0,0 @@
-public class FavoriteProgrammingLang 
-{
-    public static void main(String[] args) 
-    {
-        System.out.print("test2");
-        System.out.println("Second Print");
-    }
-}
